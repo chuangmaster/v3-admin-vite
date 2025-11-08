7b15aa66657ad825b9eda71b94fcef8ba9ff2dbb
Zander <chuang.master@gmail.com>
2025-11-09 00:10:17 +0800

    refactor: 移除不再使用的通知功能，更新語言包為繁體中文

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <script lang="ts" setup>
 import { useGreyAndColorWeakness } from "@@/composables/useGreyAndColorWeakness"
-import { usePany } from "@@/composables/usePany"
 import { useTheme } from "@@/composables/useTheme"
-import zhCn from "element-plus/es/locale/lang/zh-cn" // Element Plus 中文包
+import zhTw from "element-plus/es/locale/lang/zh-tw" // Element Plus 中文包
 
 const { initTheme } = useTheme()
 
 const { initGreyAndColorWeakness } = useGreyAndColorWeakness()
-
-const { initStarNotification, initStoreNotification, initMobileNotification } = usePany()
 
 // 初始化主题
 initTheme()
@@ -16,17 +13,11 @@ initTheme()
 // 初始化灰色模式和色弱模式
 initGreyAndColorWeakness()
 
-// #region 初始化通知
-initStarNotification()
-
-initStoreNotification()
-
-initMobileNotification()
 // #endregion
 </script>
 
 <template>
-  <el-config-provider :locale="zhCn">
+  <el-config-provider :locale="zhTw">
     <router-view />
   </el-config-provider>
 </template>
